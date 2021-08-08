a2b20baab6d963193db4c94411eb1d05b323e476
Tymur Foshch <antariuz@gmail.com>
2021-08-08 13:05:52 +0300

    done(maybe)

--- src/ThirdExercise.java
+++ src/ThirdExercise.java
@@ -5,8 +5,7 @@ public class ThirdExercise {
     public static void main(String[] args) {
         Fallout fallout = new Fallout();
         int[] k = {0, 1, 0, 3, 1, 0, 1, 3, 2, 1, 2, 1};
-        int result = fallout.solve(k);
-        System.out.println(result);
+        System.out.println(fallout.solve(k));
     }
 
 }
